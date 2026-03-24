70845ee8fbe9684df05a8a9eee345ab04da81bbd
Sumit-s2312 <sumitsongh160@gmail.com>
2026-03-24 09:05:53 +0530

    update the vite.conf file

--- apps/React-app/vite.config.js
+++ apps/React-app/vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173
+  },
+  preview:{
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: ["frontend.xode-sumit.me"]
   }
 })
